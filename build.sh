9e6313c947612cb89b5d298bdd6d7e15d4034dc7
Anoop Sharma <asanoop24@gmail.com>
2022-01-18 23:13:45 +0530

    updated gitignore

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python setup.py sdist bdist_wheel && python -m twine upload --repository pypi dist/*
-rm -rf build dist *.egg-info
-
-# API Token
-# pypi-AgEIcHlwaS5vcmcCJGRmMmU1MjFkLWI3MjctNDYyNi05NGQxLWRhMmVmYWI1MTRkNAACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgClcuNxmGBqJLzgfuM9ImBNRZcUDfH8MCq3Kw86ohmrk
